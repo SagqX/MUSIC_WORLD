58e48f7dd391c50017bd254dd995144eb758d652
𝐒𝐀𝐆𝐐𝐗 ®™ <176986778+SagqX@users.noreply.github.com>
2025-08-16 11:09:57 +0530

    Add files via upload

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,1 +1,19 @@
+# Minimal smoke tests to ensure imports work
 
+def test_imports():
+    import config
+    from utils.downloader import YouTubeDownloader
+    from utils.queue_manager import QueueManager
+    from utils.database import Database
+    from handlers import music_handlers, user_handlers, admin_handlers
+
+def test_queue_manager():
+    from utils.queue_manager import QueueManager
+    qm = QueueManager()
+    cid = 1
+    pos = qm.add_to_queue(cid, {"title": "Test Song", "duration": "03:00", "requested_by": "Tester", "path": "/tmp/a.mp3"})
+    assert pos == 1
+    assert not qm.is_empty(cid)
+    nxt = qm.get_next(cid)
+    assert nxt["title"] == "Test Song"
+    assert qm.is_empty(cid)
